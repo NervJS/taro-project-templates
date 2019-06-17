f2b9fbecc617d1ec93dd28bbd45152d1b8c4de51
chenjiajian <798095202@qq.com>
2019-06-17 16:34:15 +0800

    Update template: wxcloud

--- wxcloud/template_creator.js
+++ wxcloud/template_creator.js
@@ -2,9 +2,18 @@ function createWhenTs (params) {
   return Boolean(params.typescript)
 }
 
+function notToChangeExt () {
+  return {
+    changeExt: false
+  }
+}
+
 const handler = {
   '/global.d.ts': createWhenTs,
-  '/tsconfig.json': createWhenTs
+  '/tsconfig.json': createWhenTs,
+  '/client/config/dev.js': notToChangeExt,
+  '/client/config/index.js': notToChangeExt,
+  '/client/config/prod.js': notToChangeExt
 }
 
 module.exports = {
