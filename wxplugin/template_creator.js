286f20e7720da1bb17a7489d6ea0c7a66b6d355d
chenjiajian <798095202@qq.com>
2019-07-18 16:11:49 +0800

    fix:create page逻辑修改

--- wxplugin/template_creator.js
+++ wxplugin/template_creator.js
@@ -9,13 +9,15 @@ const handler = {
     if (period === 'createApp') return { pageName: 'list' }
     return { setPageName: `/src/plugin/pages/${pageName}/${pageName}.jsx` }
   },
-  '/src/plugin/pages/list/list.css' ({ pageName}) {
+  '/src/plugin/pages/list/list.css' ({ period, pageName}) {
+    if (period === 'createApp') return true
     return { setPageName: `/src/plugin/pages/${pageName}/${pageName}.css` }
   }
 }
 
 const basePageFiles = [
-
+  '/src/plugin/pages/list/list.jsx',
+  '/src/plugin/pages/list/list.css'
 ]
 
 module.exports = {
