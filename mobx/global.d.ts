7a5cc82703e194f39e14857549df7779cd335d6b
Garfield Lee <openpgp@550.moe>
2019-12-30 22:30:35 +0800

    fix: 更新 TARO_ENV

--- mobx/global.d.ts
+++ mobx/global.d.ts
@@ -18,7 +18,7 @@ declare namespace JSX {
 // @ts-ignore
 declare const process: {
   env: {
-    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt';
+    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq';
     [key: string]: any;
   }
 }
