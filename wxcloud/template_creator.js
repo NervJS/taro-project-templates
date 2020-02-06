3127807471fba208bb1eb15e3eb0f2ee230f569d
chenjiajian <798095202@qq.com>
2020-02-06 16:22:21 +0800

    fix(wxcloud): fix pkg that not be converted to package.json

--- wxcloud/template_creator.js
+++ wxcloud/template_creator.js
@@ -9,18 +9,24 @@ function notToChangeExt () {
 }
 
 const handler = {
-  '/client/global.d.ts': createWhenTs,
-  '/client/tsconfig.json': createWhenTs,
-  '/client/config/dev.js': notToChangeExt,
-  '/client/config/index.js': notToChangeExt,
-  '/client/config/prod.js': notToChangeExt,
-  '/client/src/pages/index/index.jsx' ({ pageName }) {
-    return { setPageName: `/client/src/pages/${pageName}/${pageName}.jsx` }
+  "/client/global.d.ts": createWhenTs,
+  "/client/tsconfig.json": createWhenTs,
+  "/client/config/dev.js": notToChangeExt,
+  "/client/config/index.js": notToChangeExt,
+  "/client/config/prod.js": notToChangeExt,
+  "/client/src/pages/index/index.jsx"({ pageName }) {
+    return { setPageName: `/client/src/pages/${pageName}/${pageName}.jsx` };
   },
-  '/client/src/pages/index/index.css' ({ pageName}) {
-    return { setPageName: `/client/src/pages/${pageName}/${pageName}.css` }
+  "/client/src/pages/index/index.css"({ pageName }) {
+    return { setPageName: `/client/src/pages/${pageName}/${pageName}.css` };
+  },
+  "/client/pkg" () {
+    return { setPageName: `/client/package.json` };
+  },
+  "/cloud/functions/login/pkg" () {
+    return { setPageName: `/cloud/functions/login/package.json` };
   }
-}
+};
 
 const basePageFiles = [
   '/client/src/pages/index/index.jsx',
