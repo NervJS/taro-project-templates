6fe12c16cee057b1585718dbccbd48b1f8227e35
yuche <i@yuche.me>
2020-03-16 18:23:45 +0800

    feat: 模板支持 h5

--- redux/src/pages/index/index.jsx
+++ redux/src/pages/index/index.jsx
@@ -1,6 +1,3 @@
-<%if (locals.typescript) {-%>
-import { ComponentClass } from 'react'
-<%}-%>
 <%if (framework === 'react') {-%>
 import React, { Component } from 'react'
 <%} else if (framework === 'nerv') { -%>
@@ -61,20 +58,6 @@ interface <%= _.capitalize(pageName) %> {
   }
 }))
 class <%= _.capitalize(pageName) %> extends Component {
-
-  <%if (locals.typescript) {-%>
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-  <%}-%>
-  config<%if (locals.typescript) {%>: Config<%}%> = {
-    navigationBarTitleText: '首页'
-  }
-
   componentWillReceiveProps (nextProps) {
     console.log(this.props, nextProps)
   }
@@ -98,15 +81,5 @@ class <%= _.capitalize(pageName) %> extends Component {
   }
 }
 
-<%if (locals.typescript) {-%>
-// #region 导出注意
-//
-// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
-// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
-//
-// #endregion
-
-export default <%= _.capitalize(pageName) %> as ComponentClass<PageOwnProps, PageState>
-<%} else {-%>
 export default <%= _.capitalize(pageName) %>
-<%}-%>
+
