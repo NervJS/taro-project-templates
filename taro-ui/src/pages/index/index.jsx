8dc8cf90d1c8363cbb47746f347b9ce8e9a80944
huangli47 <huangli@ECCMAC-C02WG2J8.local>
2020-07-15 10:43:34 +0800

    feat: 添加 v3 的 taro-ui 模版

---
+++ taro-ui/src/pages/index/index.jsx
@@ -0,0 +1,34 @@
+<%if (framework === 'react') {-%>
+import React, { Component } from 'react'
+<%} else if (framework === 'nerv') { -%>
+import Nerv, { Component } from 'nervjs'
+<%}-%>
+import { View, Text } from '@tarojs/components'
+import './<%= pageName %>.<%= cssExt %>
+import { AtButton } from 'taro-ui'
+
+export default class <%= _.capitalize(pageName) %> extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='<%= pageName %>'>
+        <Text>Hello world!</Text>
+        <AtButton type='primary'>I need Taro UI</AtButton>
+        <Text>Taro UI 支持 Vue 了吗？</Text>
+        <AtButton type='primary' circle={true}>支持</AtButton>
+        <Text>共建？</Text>
+        <AtButton type='secondary' circle={true}>来</AtButton>
+      </View>
+    )
+  }
+}
---
+++ taro-ui/src/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="<%= pageName %>">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+<%if (framework === 'vue3') {-%>
+import { ref } from 'vue'
+<%}-%>
+export default {
+  <%if (framework === 'vue') {-%>
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+  <%}-%>
+  <%if (framework === 'vue3') {-%>
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+  <%}-%>
+}
+</script>
