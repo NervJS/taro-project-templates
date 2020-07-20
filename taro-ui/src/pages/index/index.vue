2efefd1cab323742704c9d653c78b2c338b0d494
honlyHuang <huangliyn@gmail.com>
2020-07-20 22:43:58 +0800

    Add template: taro-ui (#18)
    * feat: 添加 taro-ui 模版
    
    Co-authored-by: huangli47 <huangli@ECCMAC-C02WG2J8.local>

---
+++ taro-ui/src/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="<%= pageName %>">
+    <text>{{ msg }}</text>
+    <AtButton
+      type="primary"
+      :on-click="handleClick"
+    >
+      I need Taro UI
+    </AtButton>
+  </view>
+</template>
+
+<script>
+<%if (framework === 'vue3') {-%>
+import { ref } from 'vue'
+<%}-%>
+import { AtButton } from 'taro-ui-vue'
+export default {
+  <%if (framework === 'vue') {-%>
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+  <%}-%>
+  <%if (framework === 'vue3') {-%>
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+  <%}-%>
+}
+</script>
---
+++ taro-ui/template_creator.js
@@ -0,0 +1,32 @@
+function createWhenTs (params) {
+  return !!params.typescript
+}
+
+const handler = {
+  '/global.d.ts': createWhenTs,
+  '/tsconfig.json': createWhenTs,
+  '/src/pages/index/index.jsx' ({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/${pageName}.jsx` }
+  },
+  '/src/pages/index/index.css' ({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/${pageName}.css` }
+  },
+  '/src/pages/index/index.vue' ({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/${pageName}.vue` }
+  },
+  '/src/pages/index/index.config.js' ({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/${pageName}.config.js` }
+  }
+}
+
+const basePageFiles = [
+  '/src/pages/index/index.jsx',
+  '/src/pages/index/index.css',
+  '/src/pages/index/index.vue',
+]
+
+module.exports = {
+  handler,
+  basePageFiles,
+  platforms: ['react', 'nerv']
+}
