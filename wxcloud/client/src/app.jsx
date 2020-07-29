c8669ab675172eae39fec34f1cab4343811b7d78
honlyHuang <huangliyn@gmail.com>
2020-07-29 15:33:50 +0800

    docs:更新云开发组件

--- wxcloud/client/src/app.jsx
+++ wxcloud/client/src/app.jsx
@@ -1,41 +1,19 @@
+<%if (framework === 'react') {-%>
+import React, { Component } from 'react'
+<%} else if (framework === 'nerv') { -%>
+import Nerv, { Component } from 'nervjs'
+<%}-%>
 <%if (locals.typescript) {-%>
-import Taro, { Component, Config } from '@tarojs/taro'
+import Taro, { Config } from '@tarojs/taro'
 <%} else { -%>
-import Taro, { Component } from '@tarojs/taro'
+import Taro from '@tarojs/taro'
 <%}-%>
 import Index from './pages/index'
 
 import './app.<%= cssExt %>'
 
-// 如果需要在 h5 环境中开启 React Devtools
-// 取消以下注释：
-// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
-//   require('nerv-devtools')
-// }
-
+<% if (framework === 'react' || framework === 'nerv') { -%>
 class App extends Component {
-
-<%if (locals.typescript) {-%>
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-<%}-%>
-  config<%if (locals.typescript) {%>: Config<%}%> = {
-    pages: [
-      'pages/index/index'
-    ],
-    window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
-    },
-    cloud: true
-  }
 
   componentDidMount () {
     if (process.env.TARO_ENV === 'weapp') {
@@ -49,13 +27,27 @@ class App extends Component {
 
   componentDidCatchError () {}
 
-  // 在 App 类中的 render() 函数没有实际作用
-  // 请勿修改此函数
+  // this.props.children 是将要会渲染的页面
   render () {
-    return (
-      <Index />
-    )
+    return this.props.children
   }
 }
+<%}-%>
+<% if (framework === 'vue') { -%>
+const App = new Vue({
+  onShow (options) {
+  },
+  render(h) {
+    // this.$slots.default 是将要会渲染的页面
+    return h('block', this.$slots.default)
+  }
+})
+<%}-%>
+<% if (framework === 'vue3') { -%>
+const App = createApp({
+  onShow (options) {},
+  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+})
+<%}-%>
 
-Taro.render(<App />, document.getElementById('app'))
+export default App
