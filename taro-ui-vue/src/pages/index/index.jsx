39105911119219bf409df4578eab85e913014f23
pengshanglong <pengshanglong@simuwang.com>
2020-08-13 11:43:42 +0800

    feat: add taro-ui-vue template

---
+++ taro-ui-vue/src/pages/index/index.jsx
@@ -0,0 +1,36 @@
+<%if (framework === 'react') {-%>
+import React, { Component } from 'react'
+<%} else if (framework === 'nerv') { -%>
+import Nerv, { Component } from 'nervjs'
+<%}-%>
+import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './<%= pageName %>.<%= cssExt %>'
+
+export default class <%= _.capitalize(pageName) %> extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='<%= pageName %>'>
+        <Text>Hello world!</Text>
+        <AtButton type='primary'>I need Taro UI</AtButton>
+        <Text>Taro UI 支持 Vue 了吗？</Text>
+        <AtButton type='primary' circle={true}>支持</AtButton>
+        <Text>共建？</Text>
+        <AtButton type='secondary' circle={true}>来</AtButton>
+      </View>
+    )
+  }
+}
---
+++ taro-ui-vue/src/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="<%= pageName %>">
+    <text>{{ msg }}</text>
+    <AtButton
+      type="primary"
+      :on-click="handleClick"
+    >
+      I need Taro UI
+    </AtButton>
+  </view>
+</template>
+
+<script>
+<%if (framework === 'vue3') {-%>
+import { ref } from 'vue'
+<%}-%>
+// 按需引入
+import { AtButton } from 'taro-ui-vue'
+import "taro-ui/dist/style/components/button.scss"
+import './<%= pageName %>.<%= cssExt %>' 
+export default {
+  <%if (framework === 'vue') {-%>
+  components: {
+    AtButton
+  },
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+  <%}-%>
+  <%if (framework === 'vue3') {-%>
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+  <%}-%>
+}
+</script>
