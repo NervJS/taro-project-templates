e66c6eeeb2dd66b485a5cd7685c1f4c4af12fb69
lemonye <lemonye@tencent.com>
2021-07-02 10:39:14 +0800

    feat(sdk): 增加h5模版

---
+++ h5-youshu/src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="<%= pageName %>">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
