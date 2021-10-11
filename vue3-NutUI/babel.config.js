4dd9bfd88319e240662f7ae5c3edd4ffee1849ee
richard1015 <51844712@qq.com>
2021-10-11 10:17:40 +0800

    feat: nutui-taro add plugin babel-plugin-import

--- vue3-NutUI/babel.config.js
+++ vue3-NutUI/babel.config.js
@@ -6,5 +6,25 @@ module.exports = {
       framework: '<%= framework %>',
       ts: <%= typescript %>
     }]
+  ],
+  plugins: [
+    [
+      "import",
+      {
+        "libraryName": "@nutui/nutui",
+        "libraryDirectory": "dist/packages/_es",
+        "camel2DashComponentName": false
+      },
+      'nutui3-vue'
+    ],
+    [
+      "import",
+      {
+        "libraryName": "@nutui/nutui-taro",
+        "libraryDirectory": "dist/packages/_es",
+        "camel2DashComponentName": false
+      },
+      'nutui3-taro'
+    ]
   ]
 }
