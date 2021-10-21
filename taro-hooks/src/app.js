9f6bfa3e35252fea371c3380ed4f1380287af487
innocces <jiangjiangjiangxi@gmail.com>
2021-10-21 20:46:17 +0800

    chore(template taro-hooks): fix import for app.css error & add type file

--- taro-hooks/src/app.js
+++ taro-hooks/src/app.js
@@ -1,6 +1,5 @@
-import { Component } from 'react';
-import './app.less';
-
+import { Component } from "react";
+import "./app.<%= cssExt %>";
 class App extends Component {
   componentDidMount() {}
 
