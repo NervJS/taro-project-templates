91b21a0c9287b6ffa8e79adfa0c4c5f7fa87ffd6
Zakary <zakarycode@gmail.com>
2022-03-18 19:36:30 +0800

    fix: umd error

--- taro-hooks/src/pages/index/index.jsx
+++ taro-hooks/src/pages/index/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import React, { useCallback } from "react";
 import { View, Text, Button, Image } from "@tarojs/components";
 import { useEnv, useNavigationBar, useModal, useToast } from "taro-hooks";
 import logo from "./hook.png";
