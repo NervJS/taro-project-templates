c28cf4eee42ae131665915303ede01b7230ecd88
chenjiajian <798095202@qq.com>
2022-05-27 14:53:14 +0800

    chore: update vuex & redux

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -2,18 +2,17 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
 
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+  })
+  : compose
 
 const middlewares = [
   thunkMiddleware
 ]
 
-if (process.env.NODE_ENV === 'development' && process.env.TARO_ENV !== 'quickapp') {
+if (process.env.NODE_ENV === 'development') {
   middlewares.push(require('redux-logger').createLogger())
 }
 
