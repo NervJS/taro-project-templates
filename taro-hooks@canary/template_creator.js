d89ec29f0f28d35149b04b32c9bb7c0d4e974404
innocces <jiangjiangjiangxi@gmail.com>
2022-09-03 14:10:43 +0800

    feat(taro-hooks@canary): add template of canary version of taro-hooks for 3.5

---
+++ taro-hooks@canary/template_creator.js
@@ -0,0 +1,36 @@
+function createWhenTs(params) {
+  return !!params.typescript;
+}
+
+const handler = {
+  '/type/global.d.ts': createWhenTs,
+  '/tsconfig.json': createWhenTs,
+  '/src/pages/index/index.jsx'({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.jsx` }
+  },
+  '/src/pages/index/index.css'({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.css` }
+  },
+  '/src/pages/index/index.vue'({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.vue` }
+  },
+  '/src/pages/index/index.config.js'({ pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.config.js` }
+  }
+}
+
+
+const basePageFiles = [
+  '/src/pages/index/index.jsx',
+  '/src/pages/index/index.vue',
+  '/src/pages/index/index.css',
+  '/src/pages/index/index.config.js'
+]
+
+
+module.exports = {
+  desc: "使用 taro-hooks canary 版本的模板",
+  handler,
+  basePageFiles,
+  platforms: ["react", "preact", "vue3"],
+};
