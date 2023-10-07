4f6b281ad2a741304911c1e3cb08d5ef07c42cc6
liuzejia <liuzejia@jd.com>
2023-10-07 23:18:07 +0800

    feat:  修改redux 模版，兼容 vite

--- redux/template_creator.js
+++ redux/template_creator.js
@@ -13,19 +13,19 @@ const handler = {
   '/types/vue.d.ts' ({ framework, typescript }) {
     return ['vue', 'vue3'].includes(framework) && !!typescript
   },
-  '/src/pages/index/index.jsx' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.jsx' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.jsx'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.jsx')
     }
   },
-  '/src/pages/index/index.css' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.css' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.css'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.css')
     }
   },
-  '/src/pages/index/index.config.js' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.config.js' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.config.js'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.config.js')
