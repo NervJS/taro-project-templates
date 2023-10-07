edd1861accb2fa7624231001f4ca830758ac6605
liuzejia <liuzejia@jd.com>
2023-10-07 15:18:57 +0800

    feat: 完成default-youshu模版兼容vite修改

--- default-youshu/config/prod.js
+++ default-youshu/config/prod.js
@@ -1,15 +1,13 @@
-module.exports = {
-  env: {
-    NODE_ENV: '"production"'
-  },
-  defineConstants: {
-  },
+<% if (typescript) {%>import type { UserConfigExport } from "@tarojs/cli";
+<%}%>
+export default {
   mini: {},
   h5: {
+    <% if (compiler !== 'vite') {%>
     /**
-     * WebpackChain 插件配置
-     * @docs https://github.com/neutrinojs/webpack-chain
-     */
+    * WebpackChain 插件配置
+    * @docs https://github.com/neutrinojs/webpack-chain
+    */
     // webpackChain (chain) {
     //   /**
     //    * 如果 h5 端编译后体积过大，可以使用 webpack-bundle-analyzer 插件对打包体积进行分析。
@@ -17,7 +15,6 @@ module.exports = {
     //    */
     //   chain.plugin('analyzer')
     //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
-
     //   /**
     //    * 如果 h5 端首屏加载时间过长，可以使用 prerender-spa-plugin 插件预加载首页。
     //    * @docs https://github.com/chrisvfritz/prerender-spa-plugin
@@ -33,5 +30,6 @@ module.exports = {
     //       postProcess: (context) => ({ ...context, outputPath: path.join(staticDir, 'index.html') })
     //     }))
     // }
+    <%}%>
   }
-}
+}<% if (typescript) {%> satisfies UserConfigExport<'<%= compiler %>'><%}%>
