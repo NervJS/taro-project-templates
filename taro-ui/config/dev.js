fa475d57c87bfb5421bb757967c3094ebbe01901
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2023-10-13 15:40:42 +0800

    feat: taro-ui 支持vite编译模式

--- taro-ui/config/dev.js
+++ taro-ui/config/dev.js
@@ -1,11 +1,12 @@
-module.exports = {
-  env: {
-    NODE_ENV: '"development"'
-  },
-  defineConstants: {
-  },
+<% if (typescript) {%>import type { UserConfigExport } from "@tarojs/cli";
+<%}%>
+export default {
+  <% if (compiler === 'webpack5') {%>logger: {
+    quiet: false,
+    stats: true
+  },<%}%>
   mini: {},
   h5: {
     esnextModules: ['taro-ui']
   }
-}
+}<% if (typescript) {%> satisfies UserConfigExport<'<%= compiler %>'><%}%>
--- taro-ui/template_creator.js
+++ taro-ui/template_creator.js
@@ -13,25 +13,25 @@ const handler = {
   '/types/vue.d.ts' ({ framework, typescript }) {
     return ['vue', 'vue3'].includes(framework) && !!typescript
   },
-  '/src/pages/index/index.jsx' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.jsx' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, `${pageName}.jsx`),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, `${pageName}.jsx`)
     }
   },
-  '/src/pages/index/index.css' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.css' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, `${pageName}.css`),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, `${pageName}.css`)
     }
   },
-  '/src/pages/index/index.vue' ({ pageName }) {
+  '/src/pages/index/index.vue' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, `${pageName}.vue`),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, `${pageName}.vue`)
     }
   },
-  '/src/pages/index/index.config.js' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.config.js' ({ pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, `${pageName}.config.js`),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, `${pageName}.config.js`)
