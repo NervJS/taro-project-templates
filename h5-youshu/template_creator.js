551e44ffa134c4eaeb5427924372e6f7f5912675
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2023-12-19 17:07:51 +0800

    feat: h5-youshu 更新到4.0

--- h5-youshu/template_creator.js
+++ h5-youshu/template_creator.js
@@ -3,23 +3,23 @@ const path = require('path')
 const SOURCE_ENTRY = '/src'
 const PAGES_ENTRY = '/src/pages'
 
-function createWhenTs (params) {
+function createWhenTs (err, params) {
   return !!params.typescript
 }
 
 const handler = {
   '/tsconfig.json': createWhenTs,
   '/types/global.d.ts': createWhenTs,
-  '/types/vue.d.ts' ({ framework, typescript }) {
+  '/types/vue.d.ts' (err, { framework, typescript }) {
     return ['vue', 'vue3'].includes(framework) && !!typescript
   },
-  '/src/pages/index/index.jsx' ({ pageName = '', pageDir = '', subPkg = '' }) {
+  '/src/pages/index/index.jsx' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, `${pageName}.jsx`),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, `${pageName}.jsx`)
     }
   },
-  '/src/pages/index/index.css' ({ pageName = '', pageDir = '', subPkg = '' }) {
+  '/src/pages/index/index.css' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, `${pageName}.css`),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, `${pageName}.css`)
