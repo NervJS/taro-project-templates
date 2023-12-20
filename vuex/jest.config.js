03dff914a13aec3965be70f433c02ff0e2735563
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2023-12-20 17:08:21 +0800

    feat: 更新vuex到4.0

--- vuex/jest.config.js
+++ vuex/jest.config.js
@@ -1,4 +1,4 @@
-<% if (['react', 'preact'].includes(framework)) {%>const defineJestConfig = require('@tarojs/test-utils-react/dist/jest.js').default<%}%><% if (framework === 'vue3') {%>const defineJestConfig = require('@tarojs/test-utils-vue3/dist/jest.js').default<%}%><% if (framework === 'vue') {%>const defineJestConfig = require('@tarojs/test-utils-vue/dist/jest.js').default<%}%>
+const defineJestConfig = require('@tarojs/test-utils-vue/dist/jest.js').default
 
 module.exports = defineJestConfig({
   testEnvironment: 'jsdom',
