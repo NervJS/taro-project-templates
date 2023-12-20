68710861eca40f90017fdabcc89c0281967cd103
liuzejia <liuzejia@jd.com>
2023-12-20 22:41:58 +0800

    feat: nutUI 更新到4.0

--- react-NutUI/src/app.js
+++ react-NutUI/src/app.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDidShow, useDidHide } from '@tarojs/taro'
 // 全局样式
-import './app.<%= cssExt %>'
+import './app.{{ cssExt }}'
 
 function App(props) {
   // 可以使用所有的 React Hooks
--- react-NutUI/template_creator.js
+++ react-NutUI/template_creator.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-function createWhenTs (params) {
+function createWhenTs (err, params) {
   return !!params.typescript
 }
 
@@ -10,27 +10,27 @@ const PAGES_ENTRY = '/src/pages'
 const handler = {
   '/tsconfig.json': createWhenTs,
   '/types/global.d.ts': createWhenTs,
-  '/types/vue.d.ts' ({ framework, typescript }) {
-    return ['vue', 'vue3'].includes(framework) && !!typescript
-  },
-  '/src/pages/index/index.jsx' ({ pageName = '', pageDir = '', subPkg = '' }) {
+  '/src/pages/index/index.jsx' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.jsx'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.jsx')
     }
   },
-  '/src/pages/index/index.css' ({ pageName = '', pageDir = '', subPkg = '' }) {
+  '/src/pages/index/index.css' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.css'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.css')
     }
   },
-  '/src/pages/index/index.config.js' ({ pageName = '', pageDir = '', subPkg = '' }) {
+  '/src/pages/index/index.config.js' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.config.js'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.config.js')
     }
-  }
+  },
+  '/_gitignore' () {
+    return { setPageName: `/.gitignore` }
+  },
 }
 
 const basePageFiles = [
@@ -43,6 +43,6 @@ module.exports = {
   desc: '使用 NutUI React 的模板',
   handler,
   basePageFiles,
-  platforms: ['react'],
+  platforms: ['React'],
   compiler: ['Webpack4', 'Webpack5']
 }
