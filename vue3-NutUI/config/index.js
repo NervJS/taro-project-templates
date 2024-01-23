d306c001eede1450c3cacd4dc0da011e1e500b40
eiinu <eiinu@qq.com>
2024-01-23 15:56:46 +0800

    fix: designWidth

--- vue3-NutUI/config/index.js
+++ vue3-NutUI/config/index.js
@@ -11,7 +11,14 @@ export default defineConfig{{#if typescript }}<'{{ to_lower_case compiler }}'>{{
   const baseConfig{{#if typescript }}: UserConfigExport<'{{ to_lower_case compiler }}'>{{/if}} = {
     projectName: '{{ projectName }}',
     date: '{{ date }}',
-    designWidth: 750,
+    designWidth (input) {
+      // 配置 NutUI 375 尺寸
+      if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+        return 375
+      }
+      // 全局使用 Taro 默认的 750 尺寸
+      return 750
+    },
     deviceRatio: {
       640: 2.34 / 2,
       750: 1,
