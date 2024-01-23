e114f65acb28a867dfa4e441794bad9655a72ef1
eiinu <eiinu@qq.com>
2024-01-23 16:53:19 +0800

    fix: vue vite

--- vue3-NutUI/config/index.js
+++ vue3-NutUI/config/index.js
@@ -42,7 +42,11 @@ export default defineConfig{{#if typescript }}<'{{ to_lower_case compiler }}'>{{
       prebundle: {
         enable: false
       }{{/if}}{{#if (eq compiler "Vite") }},
-      vitePlugins: [NutUIResolver({ taro: true })]{{/if}}
+      vitePlugins: [{
+        Components({
+          resolvers: [NutUIResolver({ taro: true })]
+        })
+      }]{{/if}}
     },{{#if (eq compiler "Webpack5") }}
     cache: {
       enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
