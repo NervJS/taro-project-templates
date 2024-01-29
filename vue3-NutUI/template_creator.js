71bf66d648fb8f1c879c0f34a353f1c737acd5a7
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2024-01-29 17:30:09 +0800

    fix(nutui-vue3): ts版本与文件路径

--- vue3-NutUI/template_creator.js
+++ vue3-NutUI/template_creator.js
@@ -4,6 +4,10 @@ function createWhenTs (err, params) {
   return !!params.typescript
 }
 
+function normalizePath (path) {
+  return path.replace(/\\/g, '/').replace(/\/{2,}/g, '/')
+}
+
 const SOURCE_ENTRY = '/src'
 const PAGES_ENTRY = '/src/pages'
 
@@ -13,14 +17,14 @@ const handler = {
   '/types/vue.d.ts': createWhenTs,
   '/src/pages/index/index.vue' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
-      setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.vue'),
-      setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.vue')
+      setPageName: normalizePath(path.join(PAGES_ENTRY, pageDir, pageName, 'index.vue')),
+      setSubPkgName: normalizePath(path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.vue'))
     }
   },
   '/src/pages/index/index.config.js' (err, { pageName = '', pageDir = '', subPkg = '' }) {
     return {
-      setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.config.js'),
-      setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.config.js')
+      setPageName: normalizePath(path.join(PAGES_ENTRY, pageDir, pageName, 'index.config.js')),
+      setSubPkgName: normalizePath(path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.config.js'))
     }
   },
   '/_eslintrc' () {
