e3e2470d2d94de10a4603438c0d76af2b8e5a6e3
ext.liuweijian3 <ext.liuweijian3@jd.com>
2025-11-07 10:09:00 +0800

    feat(ascf): 内容调整

--- ascf/src/app.config.js
+++ ascf/src/app.config.js
@@ -5,7 +5,7 @@ export default defineAppConfig({
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: 'ASCF',
     navigationBarTextStyle: 'black'
   }
 })
