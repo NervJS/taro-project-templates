9dec7e05ca45f814b54451980a6c6835a801d2dc
2extliuweijian32 <ext.liuweijian3@jd.com>
2025-11-10 17:17:03 +0800

    feat(ascf): 增加ASCF远程模板 (#145)
    * feat(ascf): 增加ASCF远程模板
    
    * feat(ascf): 调整package.json

---
+++ ascf/src/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="{{ pageName }}">
+    <text>\{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+{{#if (eq framework 'Vue3') }}
+import { ref } from 'vue'
+{{/if}}
+import './index.{{ cssExt }}'
+
+export default {
+{{#if (eq framework 'Vue') }}
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+{{/if}}
+{{#if (eq framework 'Vue3') }}
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+{{/if}}
+}
+</script>
---
+++ ascf/template_creator.js
@@ -0,0 +1,54 @@
+function createWhenTs (err, params) {
+  return !!params.typescript
+}
+
+const handler = {
+  '/tsconfig.json': createWhenTs,
+  '/types/global.d.ts': createWhenTs,
+  '/types/vue.d.ts' (err, { framework, typescript }) {
+    return ['Vue', 'Vue3'].includes(framework) && !!typescript
+  },
+  '/src/pages/index/index.jsx' (err, { pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.jsx` }
+  },
+  '/src/pages/index/index.css' (err, { pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.css` }
+  },
+  '/src/pages/index/index.vue' (err, { pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.vue` }
+  },
+  '/src/pages/index/index.config.js' (err, { pageName }) {
+    return { setPageName: `/src/pages/${pageName}/index.config.js` }
+  },
+  '/_editorconfig' () {
+    return { setPageName: `/.editorconfig` }
+  },
+  '/_env.development' () {
+    return { setPageName: `/.env.development` }
+  },
+  '/_env.production' () {
+    return { setPageName: `/.env.production` }
+  },
+  '/_env.test' () {
+    return { setPageName: `/.env.test` }
+  },
+  '/_eslintrc' () {
+    return { setPageName: `/.eslintrc` }
+  },
+  '/_gitignore' () {
+    return { setPageName: `/.gitignore` }
+  }
+}
+
+const basePageFiles = [
+  '/src/pages/index/index.jsx',
+  '/src/pages/index/index.vue',
+  '/src/pages/index/index.css',
+  '/src/pages/index/index.config.js'
+]
+
+module.exports = {
+  handler,
+  basePageFiles,
+  desc: '元服务ASCF模板（https://developer.huawei.com/consumer/cn/doc/atomic-ascf/ascf-overview）'
+}
